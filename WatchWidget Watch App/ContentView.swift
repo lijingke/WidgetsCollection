d995440cbb6b0fae341b70a8a586ae22e5a310c9
李京珂 <lijingke@hotmail.com>
2024-01-18 16:12:43 +0800

    [Add]添加Watch应用

---
+++ WatchWidget Watch App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WatchWidget Watch App
+//
+//  Created by 李京珂 on 2024/1/18.
+//  Copyright © 2024 李京珂. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchWidget Watch App/WatchWidgetApp.swift
@@ -0,0 +1,18 @@
+//
+//  WatchWidgetApp.swift
+//  WatchWidget Watch App
+//
+//  Created by 李京珂 on 2024/1/18.
+//  Copyright © 2024 李京珂. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WatchWidget_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
