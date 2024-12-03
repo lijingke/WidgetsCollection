a0f5a23e579fd9f2a1a3952769df29986f23567a
lijingke <lijingke@hotmail.com>
2024-12-03 15:44:09 +0800

    添加渐变色图片

--- WidgetsCollection/ViewController.swift
+++ WidgetsCollection/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorld: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         view.backgroundColor = .random
+        
+        // Diagonal gradient
+        let backgroundGradient = UIImage.gradientImage(
+            colors: [UIColor.white, UIColor.darkGray],
+            locations: [0.0, 1.0],
+            startPoint: CGPoint(),
+            endPoint: CGPoint(x: 1.0, y: 1.0),
+            size: view.bounds.size
+        )
+        
+        view.backgroundColor = UIColor(patternImage: backgroundGradient)
+        
+        // Horizontal gradient
+        let labelGradient = UIImage.gradientImage(
+            colors: [UIColor.red,  UIColor.green, UIColor.blue, UIColor.red],
+            locations: [0.0, 0.33, 0.66, 1.0],
+            size: helloWorld.bounds.size,
+            horizontal: true
+        )
+        helloWorld.textColor = UIColor(patternImage: labelGradient)
+        
+        // Vertical gradient
+        let buttonGradient = UIImage.gradientImage(
+            colors: [UIColor.black, UIColor.brown],
+            locations: [0.0, 1.0],
+            size: button.bounds.size
+        )
+        button.setTitleColor(UIColor(patternImage: buttonGradient), for: .normal)
     }
 
 
